67413430dc55a6d537e0f77386e2bd653c752794
rodney <rodney@synthesis.co.za>
2021-10-30 05:52:41 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "email" {
   description = "email for account"
   default     = "rodnet-ellis@hotmail.com"
 }
+
+variable "account_id" {
+  description = "account_id for account"
+}
+
+variable "api_token" {
+  description = "api_token for account"
+}
