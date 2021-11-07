73241d1572b33ff8f338d157022b0fb94ca63256
rodney ellis <rodney-ellis@hotmail.com>
2021-11-07 07:42:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -214,6 +214,15 @@ resource "cloudflare_record" "vpn" {
   type    = "CNAME"
 }
 
+resource "cloudflare_record" "ivan" {
+  zone_id = "70182c792b1a781051a72acfe20d5450"
+  name    = "ivan"
+  value   = "192.168.88.150"
+  type    = "A"
+}
+
+
+# Firewall Rules
 resource "cloudflare_filter" "southafrica" {
   zone_id = "70182c792b1a781051a72acfe20d5450"
   description = "allow southafrica"
