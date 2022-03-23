a5ba2a2293c43c38d666110390f9b5782211d921
rodney ellis <rodney-ellis@hotmail.com>
2022-03-23 07:51:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -228,6 +228,13 @@ resource "cloudflare_record" "adguard" {
   type    = "A"
 }
 
+resource "cloudflare_record" "jenkins" {
+  zone_id = "70182c792b1a781051a72acfe20d5450"
+  name    = "jenkins"
+  value   = "192.168.88.150"
+  type    = "A"
+}
+
 
 # Firewall Rules
 resource "cloudflare_filter" "southafrica" {
